341b4c2bc502ab5f70eea02f0a251022f181085e
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:28:44 +0000

    extension info file for eZ publish 3.9
    git-svn-id: http://svn.projects.ez.no/ezxajax@1374 987623e1-e36a-4299-8193-610e7486b1ba

---
+++ trunk/extension/xajax/ezinfo.php
@@ -0,0 +1,20 @@
+<?php
+class xajaxInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "xajax eZ publish integration",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU Lesser General Public License v2.1",
+            'Includes the following third-party software' => array( 'Name' => 'xajax',
+                                                                    'Version' => '0.5 beta',
+                                                                    'License' => 'GNU Lesser General Public License',
+                                                                    'More information' => 'http://www.xajaxproject.org/'
+                                                                  )
+        );
+    }
+}
+?>
